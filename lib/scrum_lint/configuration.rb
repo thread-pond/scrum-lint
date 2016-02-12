d8f45d531266e7d5d1699d532e73297c4d08bb07
Robert Fletcher <lobatifricha@gmail.com>
2016-02-12 15:43:24 -0800

    make config an object, start readying for config file

--- lib/scrum_lint/configuration.rb
+++ lib/scrum_lint/configuration.rb
@@ -1,17 +1,34 @@
 module ScrumLint
 
-  module Configuration
-    def self.new
-      OpenStruct.new(
-        board_name: 'Eng: Current',
-        task_list_names: ['Planned', 'This Sprint', 'Doing', 'In Review'],
-        done_list_matcher: /^Done.*$/,
-        project_list_names: ['Active Projects'],
-        ignored_list_names: %w(Emergent),
-        trello_developer_public_key: ENV.fetch('TRELLO_DEVELOPER_PUBLIC_KEY'),
-        trello_member_token: ENV.fetch('TRELLO_MEMBER_TOKEN'),
-      )
+  class Configuration
+
+    DEFAULT_CONFIGURATION = {
+      board_name:      'Eng: Current',
+      task_list_names: ['Planned', 'This Sprint', 'Doing', 'In Review'],
+      done_list_matcher: /^Done.*$/,
+      project_list_names: ['Active Projects'],
+      ignored_list_names: %w(Emergent),
+      trello_developer_public_key: ENV.fetch('TRELLO_DEVELOPER_PUBLIC_KEY'),
+      trello_member_token: ENV.fetch('TRELLO_MEMBER_TOKEN'),
+    }.freeze
+
+    attr_accessor(*DEFAULT_CONFIGURATION.keys)
+
+    def initialize
+      loaded_options = load_yaml_config
+      DEFAULT_CONFIGURATION.each do |key, value|
+        self.send("#{key}=", loaded_options.fetch(key, value))
+        loaded_options.delete(key)
+      end
+      raise "invalid options: #{loaded_options.keys}" if loaded_options.any?
     end
+
+  private
+
+    def load_yaml_config
+      {}
+    end
+
   end
 
 end
--- lib/scrum_lint/configurator.rb
+++ lib/scrum_lint/configurator.rb
@@ -3,6 +3,10 @@ module ScrumLint
   class Configurator
 
     def self.call
+      new.()
+    end
+
+    def call
       Trello.configure do |trello_config|
         trello_config.developer_public_key = trello_developer_public_key
         trello_config.member_token = trello_member_token
@@ -11,11 +15,11 @@ module ScrumLint
 
   private
 
-    def self.trello_developer_public_key
+    def trello_developer_public_key
       ScrumLint.config.trello_developer_public_key
     end
 
-    def self.trello_member_token
+    def trello_member_token
       ScrumLint.config.trello_member_token
     end
 
