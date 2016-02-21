a052573e6338df73f5512f5d38c6f33ed47d6dec
Robert Fletcher <lobatifricha@gmail.com>
2016-02-21 15:00:42 -0800

    allow codeclimate for webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require_relative '../lib/scrum_lint'
 
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
 
 def fake_trello_card(name: 'What card', desc: 'some desc')
   instance_double(Trello::Card, name: name, desc: desc)
