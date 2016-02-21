1f068465acaee962822c1bb1e8c01bce48dde34f
Robert Fletcher <lobatifricha@gmail.com>
2016-02-21 15:25:39 -0800

    pull out fakers into support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,15 +23,3 @@ RSpec.configure do |config|
     end
   end
 end
-
-def fake_trello_card(name: 'What card', desc: 'some desc')
-  instance_double(Trello::Card, name: name, desc: desc)
-end
-
-def fake_trello_list(name: 'Planned', cards: [])
-  instance_double(Trello::List, name: name, cards: cards)
-end
-
-def fake_trello_board(name: 'Eng: Current', lists: [fake_trello_list])
-  instance_double(Trello::Board, name: name, lists: lists)
-end
